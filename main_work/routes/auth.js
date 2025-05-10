6ea80725fb4c650a3c6e567e6a8f16077f62a085
YinYangCat <149885013+YinYangCat@users.noreply.github.com>
2025-05-10 22:17:32 +0700

    Update auth.js

--- main_work/routes/auth.js
+++ main_work/routes/auth.js
@@ -2,6 +2,10 @@ const express = require('express');
 const router = express.Router();
 const authController = require('../controllers/authController');
 
-router.post('/register', authController.register);
+router.post('/register', authController.register);//Register rount
+router.post('/login', authController.login);//Login rount
+router.get('/main', (req, res) => {
+  res.render('main'); // This looks for views/main.ejs
+});
 
 module.exports = router;
