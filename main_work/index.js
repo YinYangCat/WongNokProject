807532d9a66c314738bb74f7ab671b02cd44650e
NorcHamm <149895425+NorcHamm@users.noreply.github.com>
2025-05-12 06:46:19 +0700

    Update index.js

--- main_work/index.js
+++ main_work/index.js
@@ -5,17 +5,15 @@ const flash = require('connect-flash')
 const session = require('express-session')
 const router = express.Router();
 
-const authController = require('./controllers/authController')
+const authController = require('./controllers/authController');
 const mainController = require('./controllers/mainController')
 // const homeController = require('./controllers/homeController')
 const loginController = require('./controllers/loginController')
 const registerController = require('./controllers/registerController')
 const profileController = require('./controllers/profileController')
-const authRoutes = require('./routes/auth')
-const restaurantRoutes = require('./routes/restaurants')
-const mainRoutes = require('./routes/main');
-const searchRoutes = require('./routes/search');
-const reviewRoutes = require('./routes/review');
+const authRoutes = require('./routes/auth');
+const restaurantRoutes = require('./routes/restaurants');
+const adminRoutes = require('./routes/admin');
 app.set('view engine', 'ejs')
 // app.set('views', path.join(__dirname, 'views'))//make sure it in views
 
@@ -26,30 +24,16 @@ global.loggedIn = null
 app.use(session({
     secret: 'node secret',
     resave: false,
-    saveUninitialized: false,
-    cookie: {
-      maxAge: 1000 * 60 * 60 * 24, // 1 day
-      httpOnly: true,
-      secure: false  // set to true in production with HTTPS
-  }
+    saveUninitialized: false
 }))
 app.use(flash())
 // Serve static files from the "public" folder
-app.use(express.static(path.join(__dirname, 'public')))
+app.use(express.static(path.join(__dirname, 'public')));
 app.use(express.urlencoded({ extended: false }))
 app.use(express.json())
-
-app.use((req, res, next) => {
-  res.locals.user = req.session.user || null;
-  next();
-});//middleware
-app.use(express.static('public'));
-
 app.use('/', authRoutes)
-app.use('/', searchRoutes);
-app.use('/review', reviewRoutes);
-app.use('/restaurants', restaurantRoutes);
-app.use('/', mainRoutes);
+app.use('/', restaurantRoutes)
+app.use('/', adminRoutes);
 
 app.get('/', mainController)
 app.get('/login', loginController)
