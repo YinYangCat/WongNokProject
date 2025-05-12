5f8efe2bf4917c76fa2d3442e29835686b0e2b7e
NorcHamm <149895425+NorcHamm@users.noreply.github.com>
2025-05-12 21:18:03 +0700

    Update index.js

--- main_work/index.js
+++ main_work/index.js
@@ -5,14 +5,20 @@ const flash = require('connect-flash')
 const session = require('express-session')
 const router = express.Router();
 
-const authController = require('./controllers/authController');
+app.use(express.urlencoded({ extended: false }))
+app.use(express.json())
+
+const authRoutes = require('./routes/auth')
+const authController = require('./controllers/authController')
 const mainController = require('./controllers/mainController')
-// const homeController = require('./controllers/homeController')
 const loginController = require('./controllers/loginController')
 const registerController = require('./controllers/registerController')
 const profileController = require('./controllers/profileController')
-const authRoutes = require('./routes/auth');
-const restaurantRoutes = require('./routes/restaurants');
+const restaurantRoutes = require('./routes/restaurants')
+const mainRoutes = require('./routes/main');
+const searchRoutes = require('./routes/search');
+const reviewRoutes = require('./routes/review');
+const profileRoutes = require('./routes/profile');
 const adminRoutes = require('./routes/admin');
 app.set('view engine', 'ejs')
 // app.set('views', path.join(__dirname, 'views'))//make sure it in views
@@ -24,19 +30,32 @@ global.loggedIn = null
 app.use(session({
     secret: 'node secret',
     resave: false,
-    saveUninitialized: false
+    saveUninitialized: false,
+    cookie: {
+      maxAge: 1000 * 60 * 60 * 24, // 1 day
+      httpOnly: true,
+      secure: false  // set to true in production with HTTPS
+  }
 }))
 app.use(flash())
 // Serve static files from the "public" folder
-app.use(express.static(path.join(__dirname, 'public')));
-app.use(express.urlencoded({ extended: false }))
-app.use(express.json())
-app.use('/', authRoutes)
-app.use('/', restaurantRoutes)
-app.use('/', adminRoutes);
+app.use(express.static(path.join(__dirname, 'public')))
 
+app.use((req, res, next) => {
+  res.locals.user = req.session.user || null;
+  next();
+});//middleware
+app.use(express.static('public'));
+app.use('/uploads', express.static(path.join(__dirname, 'public/uploads')));
+app.use('/auth', authRoutes)
+app.use('/profile', profileRoutes);
+app.use('/', searchRoutes);
+app.use('/review', reviewRoutes);
+app.use('/restaurants', restaurantRoutes);
+app.use('/', mainRoutes);
+app.use('/', adminRoutes);
 app.get('/', mainController)
-app.get('/login', loginController)
+app.get('/auth/login', loginController);
 app.get('/profile', profileController)
 app.get('/register', registerController)
 
