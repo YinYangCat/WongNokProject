f9f6e4e2b7edfc44a48e78d76464f664f92f8b27
YinYangCat <149885013+YinYangCat@users.noreply.github.com>
2025-05-12 20:16:25 +0700

    Update WongnokDB.sql

--- WongnokDB.sql
+++ WongnokDB.sql
@@ -20,28 +20,15 @@ CREATE TABLE restaurants(
     avg_rating FLOAT NOT NULL,
     review_count INT NOT NULL,
     open_time TIME NOT NULL,
-    close_time TIME NOT NULL
+    close_time TIME NOT NULL,
+    respic INT,
+    CONSTRAINT fk_resphoto
+        FOREIGN KEY (respic) REFERENCES  photo(photoid)
+    
 );
 
--- 3. Category table
-CREATE TABLE category(
-    categoryid SERIAL PRIMARY KEY,
-    categoryname VARCHAR(20) NOT NULL
-);
 
--- 4. Restaurant_Category (Join Table)
-CREATE TABLE restaurant_category(
-    res_id VARCHAR(10),
-    categoryid INT,
-    CONSTRAINT fk_joinres
-        FOREIGN KEY (res_id) REFERENCES restaurants(res_id)
-        DEFERRABLE INITIALLY DEFERRED,
-    CONSTRAINT fk_joincat
-        FOREIGN KEY (categoryid) REFERENCES category(categoryid)
-        DEFERRABLE INITIALLY DEFERRED
-);
-
--- 5. Users table
+-- 3. Users table
 CREATE TABLE users(
     userid SERIAL PRIMARY KEY,
     name VARCHAR(50) NOT NULL,
@@ -51,10 +38,9 @@ CREATE TABLE users(
     profilepic INT,
     CONSTRAINT fk_profilephoto
         FOREIGN KEY (profilepic) REFERENCES photo(photoid)
-        DEFERRABLE INITIALLY DEFERRED
 );
 
--- 6. Review table
+-- 4. Review table
 CREATE TABLE review(
     reviewid SERIAL PRIMARY KEY,
     userid INT,
@@ -64,36 +50,21 @@ CREATE TABLE review(
     timestamp DATE NOT NULL,
     CONSTRAINT fk_userreview
         FOREIGN KEY (userid) REFERENCES users(userid)
-        DEFERRABLE INITIALLY DEFERRED,
+            ON DELETE CASCADE,
     CONSTRAINT fk_resreview
         FOREIGN KEY (res_id) REFERENCES restaurants(res_id)
-        DEFERRABLE INITIALLY DEFERRED
+            ON DELETE CASCADE
 );
 
--- 7. ReportedReview table
-CREATE TABLE reportedreview(
-    reportid SERIAL PRIMARY KEY,
-    reviewid INT,
-    reportedby INT,
-    reason VARCHAR(200),
-    status INT NOT NULL,
-    timestamp DATE NOT NULL,
-    CONSTRAINT fk_repreview
-        FOREIGN KEY (reviewid) REFERENCES review(reviewid)
-        DEFERRABLE INITIALLY DEFERRED,
-    CONSTRAINT fk_repuser
-        FOREIGN KEY (reportedby) REFERENCES users(userid)
-        DEFERRABLE INITIALLY DEFERRED
-);
 
--- 8. Admin table
+
+-- 5. Admin table
 CREATE TABLE admin(
     adminid SERIAL PRIMARY KEY,
     userid INT UNIQUE,
     permissionlevel INT NOT NULL,
     CONSTRAINT fk_useradmin
         FOREIGN KEY (userid) REFERENCES users(userid)
-        DEFERRABLE INITIALLY DEFERRED
 );
 
 -- CREATE SEQUENCE users_userid_seq;
